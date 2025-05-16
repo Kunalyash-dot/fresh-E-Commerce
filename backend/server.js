90ad295ee804ac03b79faf33834adad37e8ec27c
Kunalyash-dot <129615876+Kunalyash-dot@users.noreply.github.com>
2025-05-16 13:46:25 +0530

    removed frontend connection

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,13 @@ import addressRouter from './routes/address.router.js'
 import cartRouter from './routes/cart.router.js'
 import orderRouter from './routes/order.router.js'
 import uploadRouter from './routes/upload.router.js'
-import path from 'path'
-import { fileURLToPath } from 'url';
+// import path from 'path'
+// import { fileURLToPath } from 'url';
 
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 dotenv.config()
 // Connect to MongoDB
@@ -47,13 +47,17 @@ app.use('/api/cart',cartRouter);
 app.use('/api/order',orderRouter);
 app.use('/api/file',uploadRouter);
 
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
 // Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/dist')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+//   });
+// }
 
 
 // Start server
