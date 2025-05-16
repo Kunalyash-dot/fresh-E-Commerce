7eabdc043aa17fc989f6ed28ea795f6934699da9
Kunalyash-dot <129615876+Kunalyash-dot@users.noreply.github.com>
2025-05-16 12:43:52 +0530

    changes in production folder

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -48,7 +49,7 @@ app.use('/api/file',uploadRouter);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
   });
